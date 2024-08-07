190df5d098b1d02ca5060aa0a566c303b76fe263
04Shubham <kshubhamrnc@gmail.com>
2024-08-07 16:59:40 +0530

    variable understand

--- 01_Variable.js
+++ 01_Variable.js
@@ -1,1 +1,17 @@
-console.log("Shubham")
+const accountId = 21513365
+let accountEmail = "Shubham@gmail.com"
+var accountPassword = "2536525"
+accountCity = "Ranchi"
+
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "sk@gmail.com"
+accountPassword = "1253625"
+accountCity = "Ratu Road"
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
