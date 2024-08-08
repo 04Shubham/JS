7b9c2cc7d7af43c24bb4a374c1964154a0ac5eda
04Shubham <kshubhamrnc@gmail.com>
2024-08-08 17:29:37 +0530

    conversion

---
+++ 03_ConversionOperator.js
@@ -0,0 +1,32 @@
+let score = ""
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// Notes:-
+//"34" => 34 convert string into number
+//"34abc" => NaN (not a number)
+// true => 1 ; false => 0
+// null => 0
+// undefine => undefine
+// "" => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// Notes :- conversion into boolean form
+// 1 => true; 0 => flase
+// "" => fasle
+// "Shubham" => true
+
+let SomeNumber = 34
+
+let stringNumber = String(SomeNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
