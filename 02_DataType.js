740ee8ad80b8b4d0d8adb7b35c7432dab69389f5
04Shubham <kshubhamrnc@gmail.com>
2024-08-08 13:09:07 +0530

    dataType

---
+++ 02_DataType.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS Code  as newer Js
+            
+
+//  alert(3+3) //we are useing nodejs , not browser
+
+console.log(3 + 3)
+console.log("Shubham");
+
+
+let name="Shubham"   
+let age=18          
+let isLoggedIn=false  
+
+
+//number=> 2 to power 53
+//bigint
+// string=>""
+//boolean => true/false
+// null => standalone value
+// undefine=>
+// symbol => unique
+
+
+// Object
+
+
+
+console.log(typeof age); //type is number
+
+
+console.log(typeof null); // null of type is object
+console.log(typeof undefined); // undefine of type is undefine
