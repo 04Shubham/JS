dbd3c6765fb4953197499a0fa26d0026844e4a10
04Shubham <kshubhamrnc@gmail.com>
2024-08-08 22:27:02 +0530

    operation

--- 03_ConversionOperator.js
+++ 03_ConversionOperator.js
@@ -18,7 +18,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // Notes :- conversion into boolean form
 // 1 => true; 0 => flase
@@ -28,5 +28,17 @@ console.log(booleanIsLoggedIn);
 let SomeNumber = 34
 
 let stringNumber = String(SomeNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// *************** Operations *****************
+
+let value = 3 
+let negValue = -value
+// console.log(negValue);
+
+let str1 = 5
+let str2 = 6
+let str3 = str1 + str2
+console.log(str3)
